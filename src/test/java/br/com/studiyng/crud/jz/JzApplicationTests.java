83480395ebbe8d569070e02b1f7b91d7de2a8e3f
Mateus Bastos Marques <mateus.marques@AGU.GOV.BR>
2025-07-04 14:06:27 -0300

    projeto base spring boot

---
+++ src/test/java/br/com/studiyng/crud/jz/JzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studiyng.crud.jz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
